76ec99ba95b476b9b0b89278c24735382a73deca
Einar Helle <ei.he@hotmail.com>
2019-11-04 14:48:19 +0100

    Div retting - lagt til company

--- init.el
+++ init.el
@@ -1,10 +1,9 @@
 (require 'package)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
 			 ("melpa" . "https://melpa.org/packages/")))
 
-(unless package-archive-contents
-  (package-refresh-contents))
+;;(unless package-archive-contents
+;;  (package-refresh-contents))
 
 (package-initialize)
 
@@ -13,18 +12,3 @@
 
 (require 'org)
 (org-babel-load-file "~/.emacs.d/emacs-config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (helm use-package tagedit rainbow-delimiters projectile smex paredit cider clojure-mode-extra-font-locking clojure-mode csv-mode magit ox-reveal org-present zenburn-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
